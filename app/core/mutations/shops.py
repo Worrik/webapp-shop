ae955cf9620a9218e9091388cb5abbe6890e8554
Worrik <worrik@vivobook.localdomain>
2022-07-01 14:23:04 +0300

    updated web

--- app/core/mutations/shops.py
+++
@@ -1,8 +0,0 @@
-from app.core.models.shop import Shop, ShopIn, ShopModel
-
-
-async def create_shop(shop: ShopIn) -> Shop:
-    shop_obj = await ShopModel.create(
-        name=shop.name, description=shop.description
-    )
-    return Shop.from_pydantic(shop_obj)
