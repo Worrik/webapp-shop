3bbfa43ac9bbd713407205f482d348c6a06aeee3
Worrik <worrik@vivobook.localdomain>
2022-07-01 16:05:47 +0300

    fix

--- app/core/mutations/orders.py
+++ app/core/mutations/orders.py
@@ -10,7 +10,8 @@ async def create_order(info: Info, products_ids: List[int]) -> Order:
     print(info.context)
     user: UserModel = info.context["user"]
     products = await ProductModel.filter(id__in=products_ids)
-    order = await OrderModel.create(user=user, products=products)
+    order = await OrderModel.create(user=user)
+    await order.products.add(*products)
     return Order(
         id=order.id,
         user=User(**dict(user)),
