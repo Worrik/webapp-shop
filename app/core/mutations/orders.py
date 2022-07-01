dfb7b0557d8e92da3595aa6cf97220cb0f6edbf5
Worrik <worrik@vivobook.localdomain>
2022-07-01 17:16:46 +0300

    fix

--- app/core/mutations/orders.py
+++ app/core/mutations/orders.py
@@ -1,6 +1,12 @@
 from typing import List
 from strawberry.types import Info
-from app.core.models.order import Order, OrderModel, OrderProduct, OrderProductModel
+from app.core.models.order import (
+    Order,
+    OrderModel,
+    OrderProduct,
+    OrderProductModel,
+)
+from app.core.models.product import ProductModel
 
 from app.core.models.user import UserModel
 
@@ -8,12 +14,18 @@ from app.core.models.user import UserModel
 async def create_order(info: Info, order_products: List[OrderProduct]) -> Order:
     user: UserModel = info.context["user"]
     order = await OrderModel.create(user=user)
-    await OrderProductModel.bulk_create([
-        OrderProductModel(order=order, product=product.id, amount=product.val)
-        for product in order_products
-    ])
+    products_values = {p.id: p.val for p in order_products}
+    products = await ProductModel.filter(id__in=products_values)
+    await OrderProductModel.bulk_create(
+        [
+            OrderProductModel(
+                order=order, product=product, amount=products_values[product.id]
+            )
+            for product in products
+        ]
+    )
     return Order(
         id=order.id,
         user=user,
-        products=order.products.all(),
+        products=products,
     )
