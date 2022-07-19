874e352b5e1501810d2923cf9bd1cef4c7ebb07a
Worrik <vovachornand@gmail.com>
2022-07-19 18:36:52 +0300

    remove admin panel

--- app/initializer.py
+++ app/initializer.py
@@ -1,33 +1,14 @@
-import os
-import aioredis
 from fastapi.applications import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from tortoise.contrib.fastapi import register_tortoise
-from app.api.routers.admin.providers import LoginProvider
-from app.api.routers.admin.resources import register_admin
 
 from app.services.auth import telegram_auth
 from app.config.config import TORTOISE_ORM
 from app.api.graphql.schema import graphql_app
 from app.api.routers import bots
-from app.config import config
 
-from app.api.routers.admin.routes import admin_app
-
-from starlette.status import (
-    HTTP_401_UNAUTHORIZED,
-    HTTP_403_FORBIDDEN,
-    HTTP_404_NOT_FOUND,
-    HTTP_500_INTERNAL_SERVER_ERROR,
-)
-from fastapi_admin.exceptions import (
-    forbidden_error_exception,
-    not_found_error_exception,
-    server_error_exception,
-    unauthorized_error_exception,
-)
-from db_models import UserModel, AdminModel
+from db_models import UserModel
 
 
 def init(app: FastAPI) -> None:
@@ -35,7 +16,6 @@ def init(app: FastAPI) -> None:
     init_db(app)
     init_dependencies(app)
     init_middlewares(app)
-    init_admin(app)
 
 
 def init_routers(app: FastAPI) -> None:
@@ -65,39 +45,3 @@ def init_middlewares(app: FastAPI) -> None:
         allow_methods=["*"],
         allow_headers=["*"],
     )
-
-
-def init_admin(app: FastAPI):
-    register_admin(admin_app)
-
-    admin_app.add_exception_handler(
-        HTTP_500_INTERNAL_SERVER_ERROR, server_error_exception
-    )
-    admin_app.add_exception_handler(
-        HTTP_404_NOT_FOUND, not_found_error_exception
-    )
-    admin_app.add_exception_handler(
-        HTTP_403_FORBIDDEN, forbidden_error_exception
-    )
-    admin_app.add_exception_handler(
-        HTTP_401_UNAUTHORIZED, unauthorized_error_exception
-    )
-
-    @app.on_event("startup")
-    async def startup():
-        redis = aioredis.from_url(
-            config.REDIS_URL,
-            decode_responses=True,
-            encoding="utf8",
-        )
-        await admin_app.configure(
-            template_folders=["templates"],
-            providers=[
-                LoginProvider(
-                    admin_model=AdminModel,
-                )
-            ],
-            redis=redis,
-        )
-
-    app.mount("/admin-panel", admin_app)
--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,5 @@
-import datetime
 from typing import Optional
 import strawberry
-from tortoise import fields
-from fastapi_admin.models import AbstractAdmin
 
 
 @strawberry.type
@@ -12,18 +9,3 @@ class User:
     last_name: Optional[str]
     username: Optional[str]
     language_code: Optional[str]
-
-
-class AdminModel(AbstractAdmin):
-    last_login = fields.DatetimeField(
-        description="Last Login", default=datetime.datetime.now
-    )
-    email = fields.CharField(max_length=200, default="")
-    intro = fields.TextField(default="")
-    created_at = fields.DatetimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.pk}#{self.username}"
-
-    class Meta:
-        table = "admins"
